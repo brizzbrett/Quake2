8a71096f792368a30b782992343e6c8e14113b66
bma26 <bma26@njit.edu>
2015-02-11 18:25:44 -0500

    Changed HUD

--- p_hud.c
+++ p_hud.c
@@ -379,7 +379,7 @@ void G_SetStats (edict_t *ent)
 	{
 		item = &itemlist[ent->client->ammo_index];
 		ent->client->ps.stats[STAT_AMMO_ICON] = gi.imageindex (item->icon);
-		ent->client->ps.stats[STAT_AMMO] = ent->client->pers.inventory[ent->client->ammo_index];
+		ent->client->ps.stats[STAT_AMMO] = 100;
 	}
 	
 	//
